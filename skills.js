1fb0ab35a1cf017b24280ff2c53c559e2dbcc2bd
Prabakaran Sigamanai <prabakaransigamani94@gmail.com>
2025-03-06 10:17:55 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,9 @@
+/**
+ * This function calculates the sum of two numbers.
+ * @param {number} var1 - The first number.
+ * @param {number} var2 - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function calculateNumbers(var1, var2) {
+  return var1 + var2;
+}
